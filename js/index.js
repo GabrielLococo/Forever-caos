e81c7b1750b16be2be45740e0cf707d0e557b73f
Gabriel Lococo <127756253+GabrielLococo@users.noreply.github.com>
2023-06-18 18:34:26 -0300

    segunda pre-entrega

--- js/index.js
+++ js/index.js
@@ -1,47 +1,4 @@
 
-// class Compra {
-//     constructor(carritoDeCompras) {
-//         this.carrito = carritoDeCompras
-//     }
-//     obtenerSubtotal() {
-//         if (this.carrito.length > 0) {
-//             return this.carrito.reduce((acc, prenda)=> acc + prenda.precio, 0)
-//         }
-//     }
-// }
-
-// function buscarPrendas(codigo){
-//     let res = prendas.find((prenda)=> prenda.codigo === parseInt(codigo))
-//     return res 
-// }
-
-// function finalizarCompra() {
-//     const efectuarCompra = new Compra(carrito)
-//     alert(`Ok!, El costo total de tu compra es $ ${efectuarCompra.obtenerSubtotal()}. Muchas gracias por tu compra!`)
-// }
-
-
-// function comprar() {
-//     let codigo = prompt(`Hola, ingresa el codigo de la prenda que desees`)
-//     let prendaElegida = buscarPrendas(codigo)
-//     if (prendaElegida !== undefined) {
-//         carrito.push(prendaElegida)
-//         alert(`Has agregado "${prendaElegida.articulo}" al carrito!`)
-//         let respuesta = confirm('Deseas seguir comprando?')
-//         if (respuesta === true) {
-//             comprar()
-//         } else {
-//             finalizarCompra()
-//         }
-//     } else {
-//         alert ('El codigo ingresado no es válido, Vuelve a intentarlo por favor.')
-//         comprar()
-//     }
-// }
-
-// comprar()
-
-
 const container = document.getElementById('container')
 
 function retornoCardHTML(prenda) {
